6cc9cf8f64199f5e3b7b81feec50168dfe3fb079
Anna Andersson <anna.andersson@hotmail.com>
2018-04-14 19:08:32 +0200

    Added GUI main class.

--- Lindköping/src/ananders6/GUI.java
+++ Lindköping/src/ananders6/GUI.java
@@ -1,17 +1,54 @@
 package ananders6;
 
 import javax.swing.JFrame;
-public class GUI
-{
- public static void main(String[] args)
- {
- final int FRAME_WIDTH = 300;
- final int FRAME_HEIGHT = 400;
+import javax.swing.JButton;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
- JFrame frame = new JFrame(); // 1
- frame.setSize(FRAME_WIDTH, FRAME_HEIGHT); // 2
- frame.setTitle("Ett enkelt fönster"); // 3
- frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); // 4
- frame.setVisible(true); // 5
- }
+public class GUI extends JFrame implements ActionListener {
+
+  
+  private static final long serialVersionUID = 1L;
+  private static final int  FRAME_WIDTH      = 300;
+  private static final int  FRAME_HEIGHT     = 100;
+  private static final int  TEXT_WIDTH       = 20;
+  private JButton           button           = new JButton("Add Customer");
+  private JLabel            label            = new JLabel("Hello World");
+  private JLabel            nameLabel        = new JLabel("Name: ");
+  private JLabel            lastnameLabel    = new JLabel("Lastname: ");
+  private JLabel            pNrLabel         = new JLabel("pNr: ");
+
+  
+  
+  public GUI() {
+    createComponents();
+    setSize(FRAME_WIDTH, FRAME_HEIGHT);
+
+  }
+
+  private void createComponents() {
+
+    JPanel panel = new JPanel();
+    panel.add(button);
+    panel.add(label);
+    button.addActionListener(this);
+    this.add(panel);
+
+    JTextField nameField = new JTextField(TEXT_WIDTH);
+    panel.add(nameField);
+
+  }
+
+  @Override
+  public void actionPerformed(ActionEvent e) {
+
+  }
+
+  private void addCustomerText() {
+    return;
+  }
 }
+  
---
+++ Lindköping/src/ananders6/GUImain.java
@@ -0,0 +1,14 @@
+package ananders6;
+
+import javax.swing.JFrame;
+
+public class GUImain {
+
+  public static void main(String[] args) {
+    JFrame frame = new GUI();
+    frame.setTitle("BankClass");
+    frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    frame.setVisible(true);
+  }
+
+}
